d4c6ce3f11d42d71fe532803f114c7b68e1b7bd5
hesham <heshamelmasry1992@gmail.com>
2020-04-07 16:10:25 +0200

    initial commit, add products mocked api and show products with a product list component

---
+++ src/components/ProductList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Product List</h1>
+    <ul>
+      <li v-for="product in products"> {{product.title}} - {{product.price}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import shop from '../api/shop'
+
+  export default {
+    name: 'ProductList',
+    data() {
+      return {
+        products: []
+      }
+    },
+    created() {
+      shop.getProducts(products => {
+        this.products = products
+      })
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
