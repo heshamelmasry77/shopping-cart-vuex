605085230611962482fc43ea6eff3c5732b75953
hesham <heshamelmasry1992@gmail.com>
2020-04-08 21:31:59 +0200

    separate the actions i a separate file

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
-import shop from "../api/shop";
+import actions from "./actions";
 
 Vue.use(Vuex);
 // Creating vuex store
@@ -46,63 +46,7 @@ export default new Vuex.Store({ // State, Mutations, Getters, Actions and Module
       }
     }
   },
-  actions: { // = methods
-    fetchProducts(context) { // context object exposes the same set of methods and properties as a store object
-      // context.commit  // context.state =>> to access the state
-
-      // make the api call
-      // run setProducts mutation
-
-      // shop.getProducts(products => { // updating the state using mutation
-      //   // this.products = products
-      //   // commit  a mutation then we pass the name of the mutation
-      //   context.commit('setProducts', products)
-      // })
-
-// using promises
-      return new Promise((resolve, reject) => {
-// make the call
-        // call setProducts mutation
-        shop.getProducts(products => { // updating the state using mutation
-          // this.products = products
-          // commit  a mutation then we pass the name of the mutation
-          context.commit('setProducts', products)
-          resolve()
-        })
-      })
-    },
-
-    addProductToCart(context, product) { // when the user adds a product to the cart
-      if (product.inventory > 0) { // check if the product is in the stock
-
-        // find the item whose id is equal to the given product id
-        // check in the cart if no product with this product id in the cart
-        const cartItem = context.state.cart.find(item => item.id === product.id);
-        // find cartItem
-        if (!cartItem) {// if no product with this id in the cart
-          // push productToCart // then add this product to the cart
-          context.commit('pushProductToCart', product.id) // we commit mutation
-
-        } else { // the cartItem exist we will increment the item quantity (product exist in the cart)
-          // incrementItemQuantity  // cartItem :{id, quantity}
-          context.commit('incrementItemQuantity', cartItem) // we commit mutation
-        }
-        context.commit('decrementProductInventory', product) // reduce product inventory by 1
-      }
-    },
-    checkout({state, commit}) {
-      shop.buyProducts(
-        state.cart,
-        () => { // success call
-          commit('emptyCart') // make the cart empty
-          commit('setCheckoutStatus', 'success') // show a success message
-        },
-        () => {
-          commit('setCheckoutStatus', 'fail') // show a fail message
-        }
-      )
-    }
-  },
+  actions: actions, // = methods
   mutations: { // = are responsible for setting and updating the state ONLY
     setProducts(state, products) { // products is the payload
       // update products
