c209ca669f3ecf373fc516d044f619ad743c715a
hesham <heshamelmasry1992@gmail.com>
2020-04-08 12:36:02 +0200

    1. register the store globally. 2. remove the store from the product list component.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App'
+import store from './store'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store: store, // using the store globally
   render: h => h(App)
 })
+// we access the store this.store
