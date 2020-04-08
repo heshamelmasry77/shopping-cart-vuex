1a7720862429945a7ab529df9c4fc2a9f699b377
hesham <heshamelmasry1992@gmail.com>
2020-04-08 17:57:07 +0200

    add filter price and calculate the total add shopping cart, register filter and use it

--- src/store/index.js
+++ src/store/index.js
@@ -14,6 +14,30 @@ export default new Vuex.Store({ // State, Mutations, Getters, Actions and Module
     availableProducts(state, getters) {
       // ... return for the products array
       return state.products.filter(product => product.inventory > 0)
+    },
+    cartProducts(state, getters) {
+      return state.cart.map(cartItem => {
+        const product = state.products.find(product => product.id === cartItem.id)
+        console.log(product)
+        return {
+          title: product.title,
+          price: product.price,
+          quantity: cartItem.quantity // how many of this specific item
+        }
+      })
+    },
+    cartTotal(state, getters) {
+      // getters.cartProducts.forEach(product => {
+      //   total += product.price * product.quantity // total = total + (product.price * product.quantity)
+      // })
+      // return total
+      // another way
+      return getters.cartProducts.reduce((total, product) => {
+        console.log(total)
+        console.log(product.price)
+        console.log(product.quantity)
+        return total + (product.price * product.quantity)
+      }, 0)
     }
   },
   actions: { // = methods
@@ -65,6 +89,7 @@ export default new Vuex.Store({ // State, Mutations, Getters, Actions and Module
     setProducts(state, products) { // products is the payload
       // update products
       state.products = products
+      console.log(state)
     },
     pushProductToCart(state, productId) {
       state.cart.push({
